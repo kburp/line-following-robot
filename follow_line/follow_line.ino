3854b757d272a3e7e1ef429508ecb2a821be0b63
kburp <kburpee@olin.edu>
2022-10-03 18:27:21 -0400

    Expanded serial commands and adjusted sensor threshold

--- follow_line/follow_line.ino
+++ follow_line/follow_line.ino
@@ -9,8 +9,8 @@ int left_sensor_pin = A1;
 Adafruit_DCMotor *right_motor = AFMS.getMotor(1);
 Adafruit_DCMotor *left_motor = AFMS.getMotor(2);
 
-int forward_speed = 150;
-int sensor_threshold = 512;
+int forward_speed = 0;
+int sensor_threshold = 900;
 
 void setup() {
   Serial.begin(9600);
@@ -31,8 +31,20 @@ void loop() {
       Serial.println(forward_speed);
       
     }
-    else if (input == 115) {
+    else if (input == 115 && forward_speed > 0) {
       forward_speed--;
+      Serial.print("Speed: ");
+      Serial.println(forward_speed);
+    }
+    else if (input == 48) {
+      forward_speed = 0;
+      Serial.println("Stopped");
+    }
+    else if (input == 114) {
+      forward_speed = 150;
+      Serial.println("Speed Reset");
+    }
+    else if (input == 99) {
       Serial.print("Speed: ");
       Serial.println(forward_speed);
     }
